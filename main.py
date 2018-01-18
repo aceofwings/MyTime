f4b3ff4a00a8897fe9afc1e9a0c3794e73687a72
Phantom <dan303h@gmail.com>
2018-01-18 06:11:45 -0800

    First Commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import pygame
+
+pygame.init()
+pygame.font.init()
+screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+
+done = False
+
+font = pygame.font.Font('Fixation.ttf', 20)
+
+rendering = font.render("Love", 1, (255,255,0),(255,0,0))
+
+textrect = rendering.get_rect()
+textrect.centerx = screen.get_rect().centerx
+textrect.centery = screen.get_rect().centery
+
+screen.fill((255, 255, 255))
+screen.blit(rendering, textrect)
+
+
+while not done:
+    pygame.display.flip()
+    for event in pygame.event.get():
+        pass
+
+
+#
+# pygame.display.set_caption('font example')
+# size = [640, 480]
+# screen = pygame.display.set_mode(size)
+#
+# clock = pygame.time.Clock()
+#
+# basicfont = pygame.font.SysFont(None, 48)
+# text = basicfont.render('Hello World!', True, (255, 0, 0), (255, 255, 255))
+# textrect = text.get_rect()
+# textrect.centerx = screen.get_rect().centerx
+# textrect.centery = screen.get_rect().centery
+#
+# screen.fill((255, 255, 255))
+# screen.blit(text, textrect)
+#
+# pygame.display.update()
+#
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             sys.exit()
+#
+#     clock.tick(20)
